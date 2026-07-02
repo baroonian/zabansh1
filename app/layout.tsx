eeae03a19c51fc6d0736aaf8e5994f6908651df2
mhbaroon <baroonian@yahoo.com>
2026-07-02 08:29:33 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'English Shadowing | یادگیری زبان',
-  description: 'یادگیری زبان انگلیسی با روش سایه‌نشینی',
+  title: 'English Shadowing | یادگیری زبان به روش سایه خوانی',
+  description: 'یادگیری آسان زبان بدون تلاش',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
